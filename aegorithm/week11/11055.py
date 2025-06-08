eb9f148b65c8caec3ac75e31d211c4e1709166d6
Jieun Park <107592904+jieunyy@users.noreply.github.com>
2025-06-08 18:11:03 +0900

    BOJ11055(실버2, 30분)

---
+++ aegorithm/week11/11055.py
@@ -0,0 +1,14 @@
+# 백트래킹: 2^n, 2^1000 > 100,000,0000
+# dp:
+# 이전 요소보다 큰지 검사
+def dynamic_programming(dp, n, a):
+    for i in range(n):
+        for j in range(i):
+            if a[j] < a[i]:
+                dp[i] = max(dp[i], dp[i]+a[j])
+
+n = int(input())
+a = list(map(int, input().split()))
+dp = a[:]
+dynamic_programming(dp, n, a)
+print(max(dp))
